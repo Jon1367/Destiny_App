bd0bc8f864133efd65f1d8b8209727a1f8b2a965
Jonathan Ortiz <jonathano1367@yahoo.com>
2015-03-09 08:32:10 -0700

    asdfasdfas

--- My_First_Node_js_Program/hello.js
+++ My_First_Node_js_Program/hello.js
@@ -12,8 +12,8 @@ var ejs = require('ejs');
 var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
-
-var engine = require('./views')
+// view files
+//var engine = require('./views')
 
 // User Data
 var membershipId = 0;
@@ -23,6 +23,12 @@ var gamerTag = '';
 var characterOne = [];
 var characterTwo = [];
 var characterThree = [];
+
+// Face Book
+// AppId: 804379889616035
+// AppSerect: 3662bc272f3c540efa827618188e17a5
+
+var faceBookName = "";
 
 // Connecting to Data Base
 var mysql      = require('mysql');
@@ -34,30 +40,6 @@ var connection = mysql.createConnection({
   password : 'root',
 });
 
-// LVL GScore Class  Race Sex  Embem Path background path 
-
-// Need to store Gamer Tag & member id & membership 
-
-// Connect 
-// connection.connect();
-
-// var username = 'jonathan'
-
-// // SQL Query
-// connection.query('SELECT username from users where username = ?',[username],function(err, rows) {
-//   console.log("hello"); 
-//   console.log(rows);
-// });
-
-
-
-// Face Book
-
-// AppId: 804379889616035
-//
-// AppSerect: 3662bc272f3c540efa827618188e17a5
-
-
 
 //App Sets
 app.set('views', __dirname);
@@ -67,7 +49,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(session({secret: 'ssshhhhh'}));
 app.use(express.static(__dirname + '/public'));
-// passport initlzr
+
+// passport initlze
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -80,6 +63,7 @@ passport.use(new FacebookStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
   	process.nextTick(function() {
+  	console.log(profile);
     done(null, profile);
   });
 }));
@@ -93,20 +77,19 @@ passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
  
-
-//API
-unirest.get('http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/2/SoloFade')
-    .type('json')
-    .end(function (response) {
-        console.log(response.body);
-        data = response.body
-        //var res = JSON.parse(data);
-        console.log(data["Response"][0]["displayName"]);
+// //API
+// unirest.get('http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/2/SoloFade')
+//     .type('json')
+//     .end(function (response) {
+//         console.log(response.body);
+//         data = response.body
+//         //var res = JSON.parse(data);
+//         console.log(data["Response"][0]["displayName"]);
     
         
-});
+// });
 
- 
+
 //Routes
 
 app.get('/', function(req,res){
@@ -121,9 +104,9 @@ app.get('/login', function(req, res) {
 
 });
 
+//  Face Book Authenication
 app.get('/auth/facebook', passport.authenticate('facebook'));
 
- 
 app.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: '/success',
   failureRedirect: '/error'
@@ -138,26 +121,18 @@ app.get('/error', function(req, res, next) {
 });
 
 
-
 app.post('/processLogin', function(req, res) {
 
 	var name = req.body.name;
 	var password = req.body.password;
 
-
-
 	connection.connect();
-
-// SQL Query
+	// SQL Query
  	connection.query('SELECT username from users where username = ? and password = ?',[name,password],function(err, rows) {
    		console.log("hello"); 
    		console.log(rows);
 	 });
-
- 	connection.end();
-	
-
-	
+ 	connection.end();	
     //res.render('layout');
      res.send('<p> '+  name + password +'</p>');
 
@@ -172,139 +147,110 @@ app.post('/processApi', function(req, res) {
 	console.log(gamerTag);
 	console.log(system);
 
-
 	//API  PS4 = 2 Xbox = 1     ex. SoloFade
 	unirest.get('http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/'+system+'/'+gamerTag)
     .type('json')
     .end(function (response) {
        console.log(response.body);
-       	data = response.body;
-        //var res = JSON.parse(data);
+       data = response.body;
        gamerTag = data["Response"][0]["displayName"];
        membershipId = data["Response"][0]["membershipId"];
        
 
     });
+    console.log(gamerTag);
+    console.log(membershipId);
 
-    
+    //res.redirect('/api2');
+   	res.writeHead(301,
+  	{Location: '/api2'}
+	);
+	res.end();
+});
     //res.send(membershipId);
-
-
-
-
     // TigerPSN TigerXbox
-
     // http://www.bungie.net/Platform/Destiny/TigerPSN/Account/4611686018428490430
-
-    if(membershipId !=0){
+app.get('/api2', function(req, res) {
     
-	    unirest.get('http://www.bungie.net/Platform/Destiny/TigerPSN/Account/'+membershipId+'')
-	    .type('json')
-	    .end(function (response) {
+	unirest.get('http://www.bungie.net/Platform/Destiny/TigerPSN/Account/'+membershipId+'')
+	.type('json')
+	.end(function (response) {
 	        
-	        data = response.body
-	        characterOne = data['Response']['data']['characters'][0];
-	        characterTwo = data['Response']['data']['characters'][1];
-	        characterThree = data['Response']['data']['characters'][2];
+		data = response.body;
+		// characterOne = data['Response']['data']['characters'][0];
+	 //    characterTwo = data['Response']['data']['characters'][1];
+	 //    characterThree = data['Response']['data']['characters'][2];
 
 	        // temp[1] = data['Response']['data']['characters'][1];
 	        // temp[2] = data['Response']['data']['characters'][2];
 	        console.log(data);
 
-	        console.log(characterOne);
+	     //    console.log(characterOne);
 
-	        // Save the users
-	        characterOne[0] = characterOne["characterLevel"];
-	        characterOne[1] = characterOne["characterBase"]["grimoireScore"];
-	        characterOne[2] = characterOne["characterBase"]["genderType"];
-	       	characterOne[3] = characterOne["characterBase"]["raceHash"];
-	       	characterOne[4] = characterOne["characterBase"]["classHash"];
-	       	characterOne[5] = characterOne["emblemPath"];
-	       	characterOne[6] = characterOne["backgroundPath"];
+	     //    // Save the users
+	     //    characterOne[0] = characterOne["characterLevel"];
+	     //    characterOne[1] = characterOne["characterBase"]["grimoireScore"];
+	     //    characterOne[2] = characterOne["characterBase"]["genderType"];
+	     //   	characterOne[3] = characterOne["characterBase"]["raceHash"];
+	     //   	characterOne[4] = characterOne["characterBase"]["classHash"];
+	     //   	characterOne[5] = characterOne["emblemPath"];
+	     //   	characterOne[6] = characterOne["backgroundPath"];
 
+	     //   	characterTwo[0] = characterTwo["characterLevel"];
+	     //    characterTwo[1] = characterTwo["characterBase"]["grimoireScore"];
+	     //    characterTwo[2] = characterTwo["characterBase"]["genderType"];
+	     //   	characterTwo[3] = characterTwo["characterBase"]["raceHash"];
+	     //   	characterTwo[4] = characterTwo["characterBase"]["classHash"];
+	     //   	characterTwo[5] = characterTwo["emblemPath"];
+	     //   	characterTwo[6] = characterTwo["backgroundPath"];
 
-	       	characterTwo[0] = characterTwo["characterLevel"];
-	        characterTwo[1] = characterTwo["characterBase"]["grimoireScore"];
-	        characterTwo[2] = characterTwo["characterBase"]["genderType"];
-	       	characterTwo[3] = characterTwo["characterBase"]["raceHash"];
-	       	characterTwo[4] = characterTwo["characterBase"]["classHash"];
-	       	characterTwo[5] = characterTwo["emblemPath"];
-	       	characterTwo[6] = characterTwo["backgroundPath"];
+	     //   	characterThree[0] = characterThree["characterLevel"];
+	     //    characterThree[1] = characterThree["characterBase"]["grimoireScore"];
+	     //    characterThree[2] = characterThree["characterBase"]["genderType"];
+	     //   	characterThree[3] = characterThree["characterBase"]["raceHash"];
+	     //   	characterThree[4] = characterThree["characterBase"]["classHash"];
+	     //   	characterThree[5] = characterThree["emblemPath"];
+	     //   	characterThree[6] = characterThree["backgroundPath"];
 
-
-	       	characterThree[0] = characterThree["characterLevel"];
-	        characterThree[1] = characterThree["characterBase"]["grimoireScore"];
-	        characterThree[2] = characterThree["characterBase"]["genderType"];
-	       	characterThree[3] = characterThree["characterBase"]["raceHash"];
-	       	characterThree[4] = characterThree["characterBase"]["classHash"];
-	       	characterThree[5] = characterThree["emblemPath"];
-	       	characterThree[6] = characterThree["backgroundPath"];
-
-
-
-	       	// Displying users to check infomation
-	       	console.log(gamerTag);
-	    	console.log(characterOne[0]);
-	    	console.log(characterOne[1]);
-	    	console.log(characterOne[2]);
-	    	console.log(characterOne[3]);
-	    	console.log(characterOne[4]);
-	    	console.log(characterOne[5]);
-	    	console.log(characterOne[6]);
+	     //   	// Displying users to check infomation
+	     //   	console.log(gamerTag);
+	    	// console.log(characterOne[0]);
+	    	// console.log(characterOne[1]);
+	    	// console.log(characterOne[2]);
+	    	// console.log(characterOne[3]);
+	    	// console.log(characterOne[4]);
+	    	// console.log(characterOne[5]);
+	    	// console.log(characterOne[6]);
 
 
-	    	console.log(characterTwo[0]);
-	    	console.log(characterTwo[1]);
-	    	console.log(characterTwo[2]);
-	    	console.log(characterTwo[3]);
-	    	console.log(characterTwo[4]);
-	    	console.log(characterTwo[5]);
-	    	console.log(characterTwo[6]);
+	    	// console.log(characterTwo[0]);
+	    	// console.log(characterTwo[1]);
+	    	// console.log(characterTwo[2]);
+	    	// console.log(characterTwo[3]);
+	    	// console.log(characterTwo[4]);
+	    	// console.log(characterTwo[5]);
+	    	// console.log(characterTwo[6]);
 
-	    	console.log(characterThree[0]);
-	    	console.log(characterThree[1]);
-	    	console.log(characterThree[2]);
-	    	console.log(characterThree[3]);
-	    	console.log(characterThree[4]);
-	    	console.log(characterThree[5]);
-	    	console.log(characterThree[6]);
-
-
-
-
-		});
-	}else{
-
-		console.log('Fuck UP');
-	}
+	    	// console.log(characterThree[0]);
+	    	// console.log(characterThree[1]);
+	    	// console.log(characterThree[2]);
+	    	// console.log(characterThree[3]);
+	    	// console.log(characterThree[4]);
+	    	// console.log(characterThree[5]);
+	    	// console.log(characterThree[6]);
 
     //console.log(temp[0]);
 
-
 	// var name = req.body.name;
-	// // var password = req.params.password;
-
-
- ///      temp[0] = res.body['Response']['data']['characters'][0];
-//        temp[1] = res.body['Response']['data']['characters'][1];
-//        temp[2] = res.body['Response']['data']['characters'][2];
-
-    	res.render('./views/profile',{gamerTag : gamerTag});
-
-
- 		//res.send('<p> '+  Hello +'</p>');
+	// var password = req.params.password;
+ 	});
+    res.render('./views/profile',{gamerTag : gamerTag});
 
 });
-
-
 
 app.listen(8080);
 
 console.log("Listening on port 8080");
-
-
-
-
 
 // // How we require modules
 // var http = require('http'); 
@@ -323,4 +269,5 @@ console.log("Listening on port 8080");
 
 //  console.log("Listening on port 8080");
 
+// Desetiny Api Key
 // // 993669113bf34361902b6343a306e98e
