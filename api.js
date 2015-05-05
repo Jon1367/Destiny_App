17fc766b7eda7a2044484f861e54006019aae671
Jonathan Ortiz <jonathano1367@yahoo.com>
2015-05-05 03:13:33 -0400

    updated

--- api.js
+++ api.js
@@ -15,7 +15,7 @@ exports.apiOne = function(system,gamerTag,callback){
         
         
        //console.log(response.body)p;
-       data = response.body;
+       var data = response.body;
         console.log('length');
        console.log(typeof(data));
         console.log(data);
@@ -59,6 +59,68 @@ exports.apiOne = function(system,gamerTag,callback){
  
     
 };
+exports.friend = function(system,gamerTag,callback){
+
+  var system;
+
+  unirest.get('http://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/'+system+'/'+gamerTag)
+    .type('json')
+    .end(function (response) {
+        
+        
+       //console.log(response.body)p;
+       var data = response.body;
+        console.log('length');
+       console.log(typeof(data));
+        console.log(data);
+
+        if (isEmptyObject(data) == true) {
+            callback(55);
+            return false;      
+        } else {
+        
+           gamerTag = data["Response"][0]["displayName"];
+           membershipId = data["Response"][0]["membershipId"];
+           systemType = data["Response"][0]["membershipType"];
+
+           if(systemType = 2){
+            system = 'TigerPSN';
+           }else if(systemType = 1){
+            system = "TigerXbox";
+           }
+           console.log(systemType);
+           console.log(system);
+           console.log(membershipId);
+        }
+
+
+        unirest.get('http://www.bungie.net/Platform/Destiny/'+system+'/Account/'+membershipId+'')
+    .type('json')
+    .end(function (response) {
+//      console.log('data2');
+//      console.log(response.body);
+
+          var data2 = response.body;
+      var characters = data2['Response']['data']['characters'];
+   //    characterTwo = data['Response']['data']['characters'][1];
+   //    characterThree = data['Response']['data']['characters'][2];
+      callback(characters);
+
+    });
+
+     });
+    console.log('hello');
+ 
+    
+};
+
+
+
+
+
+
+
+
 exports.apiTwo = function(membershipId,characterId,callback){
 
     unirest.get('http://www.bungie.net/Platform/Destiny/2/Account/'+membershipId+'/Character/'+characterId+'/Inventory/')
---
+++ hello.js
@@ -0,0 +1,380 @@
+// Node.js
+
+// Includes
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var unirest = require("unirest");
+var app = express();
+var http = require('http');
+var path = require('path');
+var ejs = require('ejs');
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+// modules
+var api = require('./api.js');
+
+// global Variables
+
+// User Data
+var membershipId;
+var gamerTag = '';
+// var characterId;
+
+
+// user's character Info
+var character;
+
+var characterInfo = [];
+
+var characterOne = [];
+var characterTwo = [];
+var characterThree = [];
+
+// Friends Character Info
+var fcharacter;
+
+var fcharacterInfo = [];
+
+var fgamerTag = '';
+var fcharacterOne = [];
+var fcharacterTwo = [];
+var fcharacterThree = [];
+/*
+ 	Face Book:
+ 	AppId: 804379889616035
+ 	AppSerect: 3662bc272f3c540efa827618188e17a5
+*/
+
+var faceBookName = "";
+
+// Connecting to MYSQL Data Base
+/*
+ var mysql      = require('mysql');
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   port     :  '8889',
+   database : 'projectdb',
+   user     : 'root',
+   password : 'root',
+ });
+
+// connection.connect();
+
+*/
+
+//App Sets
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+// App Use
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'ssshhhhh'}));
+app.use(express.static(__dirname + '/public'));
+
+// passport initlze
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connecting to FaceBook 
+passport.use(new FacebookStrategy({
+    clientID: '804379889616035',
+    clientSecret: '3662bc272f3c540efa827618188e17a5',
+    callbackURL: "http://localhost:8080/auth/facebook/callback",
+    enableProof: false
+  },
+  function(accessToken, refreshToken, profile, done) {
+  	process.nextTick(function() {
+  	//console.log(profile);
+    done(null, profile);
+  });
+}));
+
+// SerializeUser
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+ 
+
+/*=============   Routes   =============*/
+
+//  Face Book Authenication
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/success',
+  failureRedirect: '/error'
+}));
+
+app.get('/success', function(req, res, next) {
+  res.redirect('/');
+});
+ 
+app.get('/error', function(req, res, next) {
+  res.send("Error logging in.");
+});
+
+/*=============   Views   =============*/
+
+app.get('/', function(req,res){
+
+	res.sendfile(path.join(__dirname + '/views/layout.html'));
+
+});
+app.post('/viewCharacter', function(req, res) {
+
+	characterchoose = req.body.character;
+
+	console.log(character);
+
+
+	if (characterchoose == 0) {
+		character = characterOne;
+		res.redirect('/chooseCharacter');
+
+	}else if(characterchoose == 1){
+		character = characterTwo;
+		res.redirect('/chooseCharacter');
+
+	}else if(characterchoose == 2){
+		character = characterThree;
+		res.redirect('/chooseCharacter');
+	}
+
+});
+app.get('/chooseCharacter', function(req, res) {
+
+	console.log(character);
+
+	res.render('./views/charView',{character:character});
+
+});
+
+app.get('/friend', function(req, res) {
+
+	res.render('./views/friend',{gamerTag:gamerTag,
+	characterOne : characterOne,
+	characterTwo:characterTwo,
+	characterThree : characterThree
+ 	});
+
+});
+app.get('/back', function(req, res) {
+
+	
+	res.render('./views/profile',{gamerTag:gamerTag,
+			characterOne : characterOne,
+			characterTwo:characterTwo,
+			characterThree : characterThree
+                                     });
+
+});
+
+/*=============   Processing   =============*/
+
+app.post('/processApi', function(req, res) {
+
+	var data;
+
+	// Form Data
+	gamerTag = req.body.gamerTag;
+	var system = req.body.system;
+
+	console.log(gamerTag);
+	console.log(system);
+
+
+    
+	api.apiOne(system,gamerTag,function(result){
+
+        console.log(typeof(result));
+//        console.log(result);
+        
+        if (result == 55) {
+            res.render('./views/errors');       
+        } else {
+        
+		data = result;
+//		console.log(data);
+        
+
+		membershipId = data[0]['characterBase']['membershipId'];
+		characterOne = data[0];
+		characterTwo = data[1];
+		characterThree = data[2];
+
+		for (var i = 0; i <= 2; i++) {
+			characterInfo.push(data[i]);
+		};
+
+		console.log("character Info Variable");
+		console.log(characterInfo);
+//		console.log(characterTwo);
+//		console.log(characterThree);
+
+
+
+			res.render('./views/profile',{gamerTag:gamerTag,
+				characterOne : characterOne,
+				characterTwo:characterTwo,
+				characterThree : characterThree
+	          });
+		
+        }
+                   
+
+	});
+});
+
+app.post('/processFriendApi', function(req, res) {
+
+	var data;
+
+	fgamerTag = req.body.gamerTag;
+	var system = req.body.system;
+
+	api.friend(system,fgamerTag,function(result){
+
+        console.log(typeof(result));
+//        console.log(result);
+        
+        if (result == 55) {
+            res.render('./views/errors');       
+        } else {
+        
+		data = result;
+		console.log(data);
+        
+
+		membershipId = data[0]['characterBase']['membershipId'];
+		fcharacterOne = data[0];
+		fcharacterTwo = data[1];
+		fcharacterThree = data[2];
+
+		for (var i = 0; i <= 2; i++) {
+			fcharacterInfo.push(data[i]);
+		};
+
+		// FRIEND API
+		console.log('FRIEND API ');
+		console.log(fcharacterOne);
+		console.log(fcharacterTwo);
+		console.log(fcharacterThree);
+
+
+		// FIRst Character
+		console.log(characterOne);
+		console.log(characterTwo);
+		console.log(characterThree);
+
+		res.render('./views/compare',{gamerTag:gamerTag,
+			characterOne : characterOne,
+			characterTwo:characterTwo,
+			characterThree : characterThree,
+			fgamerTag:fgamerTag,
+			fcharacterOne : fcharacterOne,
+			fcharacterTwo: fcharacterTwo,
+			fcharacterThree : fcharacterThree
+          });
+        }
+                   
+
+	});
+
+
+
+
+});
+app.post('/processCompare', function(req, res) {
+
+	var cOne = req.body.character
+	var cTwo = req.body.fcharacter;
+
+
+
+	console.log("character Choice");
+	console.log(cOne);
+	console.log(cTwo);
+
+
+	
+	res.render('./views/charCompare',{gamerTag:gamerTag,
+			character : characterInfo[cOne],
+			fgamerTag: fgamerTag,
+			fcharacter : fcharacterInfo[cTwo]
+        });
+
+});
+
+/*=============   DataBase CRUD unFinish   =============*/
+
+app.get('/login', function(req, res) {
+
+    res.render('./views/form');
+
+});
+
+app.get('/addUser', function(req, res) {
+
+	res.render('./views/addUser');
+
+});
+
+app.post('/processLogin', function(req, res) {
+
+	var name = req.body.name;
+	var password = req.body.password;
+
+	// sess = req.session;
+	// sess.name = name;
+	// sess.bool = true;
+    
+
+	// // SQL Query
+ // 	connection.query('SELECT username from users where username = ? and password = ?',[name,password],function(err, rows) {
+ //   		console.log("hello"); 
+ //   		console.log(rows);
+
+	//  });
+
+    
+    res.redirect('/');
+
+     
+});
+app.post('/processAdd', function(req, res) {
+
+	 var name = req.body.name;
+	 var password = req.body.password;
+     var gamertag = req.body.gamertag;
+	 var system = req.body.system;
+
+//	 sess = req.session;
+//	 sess.name = name;
+
+
+	// SQL Query
+ 	// connection.query('insert into users(username,password,gamertag,system)values(?,?,?,?)',[name,password,gamertag,system],function(err, rows) {
+  //  		console.log("hello"); 
+  //  		console.log(rows);
+	 // });
+
+
+
+    res.redirect('/');
+
+
+});
+app.get('/logout', function(req, res) {
+    sess = null;
+    
+    res.redirect('/')
+});
+
+app.listen(8080);
+console.log("Listening on port 8080");
